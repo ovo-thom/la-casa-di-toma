c1a8dbf875eebee6c3a05f0c637e9f758e203e71
Thomas OvO <thonnard.thomas@gmail.com>
2025-08-05 23:45:38 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='h-screen bg-indigo-800 text-white'>
+        Hello
+        </div>
+        
+    </>
+  )
+}
+
+export default App
